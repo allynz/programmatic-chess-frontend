63f72d4e7462393add0024cbf5c60c55d3cefeed
allynz <43417311+allynz@users.noreply.github.com>
2023-09-20 14:19:17 +0530

    adding favicon for android chrome

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -7,6 +7,7 @@ export default function Document() {
                 <link rel="apple-touch-icon" sizes="180x180" href="/favicon_io/apple-touch-icon.png" />
                 <link rel="icon" type="image/png" sizes="32x32" href="/favicon_io/favicon-32x32.png" />
                 <link rel="icon" type="image/png" sizes="16x16" href="/favicon_io/favicon-16x16.png" />
+                <link rel="icon" type="image/png" sizes="192x192" href="/favicon_io/android-chrome-192x192.png" />
                 {/* needed to use crossOrigin https://github.com/aws-amplify/amplify-hosting/issues/195 */}
                 <link crossOrigin='use-credentials' rel="manifest" href="/favicon_io/site.webmanifest" />
             </Head>
